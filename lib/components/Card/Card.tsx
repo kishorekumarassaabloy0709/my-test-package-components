f0ed99986dbc7bd0d94b8fa8d8b2d8aad01bea94
kishorekumarassaabloy0709 <kishorekumar.selvarajan@assaabloy.com>
2026-09-02 16:03:05 +0530

    test

--- lib/components/Card/Card.tsx
+++ lib/components/Card/Card.tsx
@@ -8,15 +8,25 @@ export interface CardProps
 }
 
 export const Card = React.forwardRef<HTMLDivElement, CardProps>(
+
   ({ className, children, ...props }, ref) => (
+
     <div
+
       ref={ref}
+
       className={`${styles.card} ${className || ''}`}
+
       {...props}
+
     >
+
       {children}
+
     </div>
+
   )
+
 );
 
 Card.displayName = 'Card';
